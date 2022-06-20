35ffe70814cd8e95d2ef156735f505d6ff46251f
Nikita Landarenko <landarenko_nikita@mail.ru>
2022-06-20 19:00:02 +0500

    Game done

---
+++ Snake/Point.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Point
+    {
+        public Point()
+        {
+
+        }
+
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.right)
+            {
+                x += offset;
+            }
+            else if (direction == Direction.left)
+            {
+                x -= offset;
+            }
+            else if (direction == Direction.up)
+            {
+                y -= offset;
+            }
+            else if (direction == Direction.down)
+            {
+                y += offset;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == x && p.y == y;
+        }
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Snake : Figure
+    {
+        Direction direction;
+        bool blockUp = false;
+        bool blockDown = false;
+        bool blockLeft = true;
+        bool blockRight = false;
+        
+        public Snake(Point tail, int length, Direction _direction)
+        {
+            direction = _direction;
+            pList = new List<Point>();
+
+            for(int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+
+        public bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow && !blockLeft)
+            {
+                blockUp = false;
+                blockDown = false;
+                blockLeft = false;
+                blockRight = true;
+                
+                direction = Direction.left;
+            }
+            else if (key == ConsoleKey.RightArrow && !blockRight)
+            {
+                blockUp = false;
+                blockDown = false;
+                blockLeft = true;
+                blockRight = false;
+
+                direction = Direction.right;
+            }
+            else if (key == ConsoleKey.UpArrow && !blockUp)
+            {
+                blockUp = false;
+                blockDown = true;
+                blockLeft = false;
+                blockRight = false;
+
+                direction = Direction.up;
+            }
+            else if (key == ConsoleKey.DownArrow && !blockDown)
+            {
+                blockUp = true;
+                blockDown = false;
+                blockLeft = false;
+                blockRight = false;
+
+                direction = Direction.down;
+            }
+        }
+
+        public bool Eat(Point food)
+        {
+            Point head = GetNextPoint();
+            if (head.IsHit(food))
+            {
+                food.sym = head.sym;
+                food.Draw();
+                pList.Add(food);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
